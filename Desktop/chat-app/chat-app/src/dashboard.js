f5f58b7646eb6984eeaa14397076ab28974f911a
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2019-12-18 12:14:50 +0000

    added window divisions

--- Desktop/chat-app/chat-app/src/dashboard.js
+++ Desktop/chat-app/chat-app/src/dashboard.js
@@ -6,8 +6,28 @@ import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(3, 2),
+      margin: '50px',
+      padding: theme.spacing(3, 2),
   },
+  flex: {
+    display: 'flex'
+
+  },
+  topicsWindow: {
+    width: '30%',
+    height: '300px'
+
+  },
+  chatWindow: {
+      width: '70%',
+      height: '300px'
+  },
+  chatBox: {
+      width: '85%'
+  },
+  button: {
+      width: '15%'
+  }
 }));
 
 export default function Dashboard() {
@@ -17,12 +37,18 @@ export default function Dashboard() {
   return (
     <div>
     <Paper className={classes.root}>
-      <Typography variant="h5" component="h3">
-        This is a sheet of paper.
+      <Typography variant="h3" component="h3">
+        Chat App
       </Typography>
-      <Typography component="p">
-        Paper can be used to build surface or other elements for your application.
+      <Typography variant="h5" component="h5">
+        Topic Placeholder
       </Typography>
+      <div className={classes.flex}>
+
+      </div>
+      <div className={classes.flex}>
+
+      </div>
     </Paper>
     </div>
   )
