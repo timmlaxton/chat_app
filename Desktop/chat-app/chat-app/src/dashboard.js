edc9b5a1daac066862012ed02849ed0f6d97af6c
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2019-12-19 20:11:52 +0000

    added chat window topics window chimp

--- Desktop/chat-app/chat-app/src/dashboard.js
+++ Desktop/chat-app/chat-app/src/dashboard.js
@@ -4,8 +4,8 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Chip from '@material-ui/core/Chip';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -49,10 +49,26 @@ export default function Dashboard() {
       </Typography>
       <div className={classes.flex}>
         <div className={classes.topicsWindow}>
+        <List>
+        {
+          ['topic'].map(topic => (
+            <ListItem key={topic} button>
+              <ListItemText primary={topic} />
+            </ListItem>
+          ))
+        }
 
+        </List>
       </div>
       <div className={classes.chatWindow}>
-
+      {
+        [{from: 'user', msg: 'alright'}].map((chat, i) => (
+          <div className={classes.flex} key={i}>
+          <Chip label={chat. from} className={classes.chip} />
+          <Typography variant='p'>{chat.msg}</Typography>
+          </div>
+        ))
+      }
       </div>
       </div>
       <div className={classes.flex}>
