a2d24fe8d7a1b3c976f2d1c3adf2e1307e253309
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-03-22 17:27:26 +0000

    server logs in users and creates tokens

--- Desktop/shop/server/server.js
+++ Desktop/shop/server/server.js
@@ -31,6 +31,28 @@ app.post('/api/users/register', (req, res)=> {
         })
     })
 
+});
+
+app.post('/api/users/login',(req,res)=>{
+    
+    
+    User.findOne({'email':req.body.email},(err,user)=>{
+        if(!user) return res.json({loginSuccess:false,message:"Authentication failed, email not found"});
+
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+            if(!isMatch) return res.json({loginSuccess:false,message:'Password incorrect'});
+
+            user.generateToken((err, user)=>{
+                if(err) return res.status(400).send(err);
+                res.cookie('w_auth',user.token).status(200).json({
+                    loginSuccess: true
+                })
+
+            })
+
+        })
+    })
+
 })
 
 
