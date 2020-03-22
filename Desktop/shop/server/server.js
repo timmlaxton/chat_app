4bd223f46aef5c1aab457de0d666ee4d7906dc7d
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-03-22 14:44:12 +0000

    server extended

--- Desktop/shop/server/server.js
+++ Desktop/shop/server/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE)
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 
 const port = process.envPORT || 3002;
 
