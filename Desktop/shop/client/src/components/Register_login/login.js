71001add7b4b2614481acba18be4641eef7c68e3
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-03-28 15:04:25 +0000

    added user login

--- Desktop/shop/client/src/components/Register_login/login.js
+++ Desktop/shop/client/src/components/Register_login/login.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import FormField from '../utils/Form/formfield';
-import {update} from '../utils/Form/formActions';
+import {update, generateData, isFormValid} from '../utils/Form/formActions';
+import {withRouter} from 'react-router-dom';
+
 
 import { connect } from 'react-redux';
-
+import {loginUser} from '../../actions/user_actions';
 
 class Login extends Component {
 
@@ -63,8 +65,39 @@ updateForm = (element) => {
 }
 
 
-submitForm = () => {
+submitForm = (event) => {
+    event.preventDefault();
 
+    let dataToSubmit = generateData(this.state.formdata, 'login');
+    let formIsValid = isFormValid(this.state.formdata, 'login')
+
+    if(formIsValid){
+        this.props.dispatch(loginUser(dataToSubmit)).then(response => {
+            if(response.payload.loginSuccess){
+                console.log(response.payload);
+                this.props.history.push('/user/dashboard')
+                
+
+            }else{
+                this.setState({
+                    formError: true
+                })
+
+            }
+        });
+
+
+    } else {
+        this.setState({
+            formError: true
+        })
+    }
+
+
+
+
+  
+    
 
 }
 
@@ -82,12 +115,24 @@ submitForm = () => {
                 
                 />
 
-<FormField
+                <FormField
                     id={'password'}
                     formdata={this.state.formdata.password}
                     change={(element)=> this.updateForm(element)}
                 
                 />
+
+                {this.state.formError ?
+                <div className="error_label">  
+                    Please check your data
+                </div>
+                :null}
+
+                <button onClick={(event) => this.submitForm(event)}>
+                    Sign in
+                </button>
+
+
 
 
                 </form>
@@ -97,4 +142,4 @@ submitForm = () => {
     }
 }
 
-export default connect()(Login);
+export default connect()(withRouter(Login));
